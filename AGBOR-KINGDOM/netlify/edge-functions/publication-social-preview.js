a0820cf4b80faf976c41b817fd921f477608812b
ucrecordstudio <ucrecordstudio@gmail.com>
2026-09-10 05:16:51 -0700

    upadte

--- AGBOR-KINGDOM/netlify/edge-functions/publication-social-preview.js
+++ AGBOR-KINGDOM/netlify/edge-functions/publication-social-preview.js
@@ -41,6 +41,9 @@ export default async function (request, context) {
         const supabaseAnonKey =
             Netlify.env.get("SUPABASE_ANON_KEY");
 
+            console.log("SUPABASE_URL available:", !!supabaseUrl);
+console.log("SUPABASE_ANON_KEY available:", !!supabaseAnonKey);
+
         if (
             !supabaseUrl ||
             !supabaseAnonKey
